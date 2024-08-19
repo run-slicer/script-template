5fc5428b7f739757a1e7609f51b9b1af0e3200ab
Matouš Kučera <mk@kcra.me>
2024-08-19 13:26:38 +0200

    fix: add Node resolving plugin

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,6 +2,7 @@ import { readFile } from "node:fs/promises";
 import type { RollupOptions } from "rollup";
 import typescript from "@rollup/plugin-typescript";
 import replace from "@rollup/plugin-replace";
+import nodeResolve from "@rollup/plugin-node-resolve";
 
 const { version } = JSON.parse(await readFile("./package.json", "utf8"));
 
@@ -12,6 +13,7 @@ export default {
         format: "esm",
     },
     plugins: [
+        nodeResolve(),
         typescript(),
         replace({
             preventAssignment: true,
